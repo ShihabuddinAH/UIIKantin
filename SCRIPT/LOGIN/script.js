bab9493f3a3baf813ec3639bc8968bfc0d0b73a4
ShihabuddinAH <23523003@students.uii.ac.id>
2024-12-25 23:22:21 +0700

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   ASSETS/KANTIN/UIIKantin.png 	new file:   ASSETS/MENU/UIIKantin.png 	new file:   ASSETS/MENU/shopping-cart.png 	modified:   SCRIPT/LOGIN/script.js

--- SCRIPT/LOGIN/script.js
+++ SCRIPT/LOGIN/script.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
   buyerToggle.addEventListener("click", () => {
     buyerToggle.classList.add("active");
     sellerToggle.classList.remove("active");
-    signupLink.href = "/PABW/Proyek/EXPO/PHP/PEMBELI/signupPembeli.php";
+    signupLink.href = "../../PHP/PEMBELI/signupPembeli.php";
     roleInput.value = "buyer";
   });
 
   sellerToggle.addEventListener("click", () => {
     sellerToggle.classList.add("active");
     buyerToggle.classList.remove("active");
-    signupLink.href = "/PABW/Proyek/EXPO/PHP/PENJUAL/signupPenjual.php";
+    signupLink.href = "../../PHP/PENJUAL/signupPenjual.php";
     roleInput.value = "seller";
   });
 });
